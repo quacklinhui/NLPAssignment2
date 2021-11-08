b086add2f844bd0e57ffcacf5e54efba5182b4d3
gracecheah000 <65240352+gracecheah000@users.noreply.github.com>
2021-11-08 12:13:22 +0800

    Update generate.py

--- Q1/generate.py
+++ Q1/generate.py
@@ -36,6 +36,13 @@ parser.add_argument('--log-interval', type=int, default=100,
                     help='reporting interval')
 args = parser.parse_args()
 
+
+def modify_input(input, new_word):
+    length = len(input[0])
+    result = torch.cat([input.squeeze()[1:], new_word.view(-1)])
+    result = result.view(1, length)
+    return result
+
 # Set the random seed manually for reproducibility.
 torch.manual_seed(args.seed)
 if torch.cuda.is_available():
@@ -48,21 +55,22 @@ if args.temperature < 1e-3:
     parser.error("--temperature has to be greater or equal 1e-3")
 
 with open(args.checkpoint, 'rb') as f:
-    model = torch.load(f).to(device)
+    model = torch.load(f, map_location=device).to(device)
 model.eval()
 
 corpus = data.Corpus(args.data)
 ntokens = len(corpus.dictionary)
 
-input = torch.randint(ntokens, (1, 8), dtype=torch.long).to(device)
+input = torch.randint(ntokens, (1, 7), dtype=torch.long).to(device)
 
 with open(args.outf, 'w', encoding='utf-8') as outf:
     with torch.no_grad():  # no tracking history
         for i in range(args.words):
+            print(input)
             output = model(input)
             word_weights = output.squeeze().div(args.temperature).exp().cpu()
             word_idx = torch.multinomial(word_weights, 1)[0]
-            input.fill_(word_idx)
+            input = modify_input(input, word_idx)
 
             word = corpus.dictionary.idx2word[word_idx]
 
